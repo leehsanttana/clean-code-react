b1d8d7baccbda27c0045e557d7b4ce524eba6616
Leonam Santana <leonamsanttana@gmail.com>
2022-03-31 08:16:21 -0300

    refactor: group imports in index files

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,15 +1,11 @@
 import { RemoteAuthentication } from "./remote-authentication";
-import { HttpPostClientSpy } from "../../test/mock-http-client";
-import {
-  mockAccountModel,
-  mockAuthentication,
-} from "../../../domain/test/mock-account";
-import faker from "faker";
-import { HttpStatusCode } from "@/data/protocols/http/http-response";
-import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
-import { UnexpectedError } from "@/domain/errors/unexpected-error";
+import { HttpPostClientSpy } from "@/data/test";
+import { mockAccountModel, mockAuthentication } from "@/domain/test/";
+import { HttpStatusCode } from "@/data/protocols/http/";
+import { InvalidCredentialsError, UnexpectedError } from "@/domain/errors/";
 import { AuthenticationParams } from "@/domain/usecases/Authentication";
 import { AccountModel } from "@/domain/models/AccountModel";
+import faker from "faker";
 
 type SutTypes = {
   sut: RemoteAuthentication;
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,12 +1,7 @@
-import { HttpPostClient } from "@/data/protocols/http/http-post-client";
-import { HttpStatusCode } from "@/data/protocols/http/http-response";
-import { InvalidCredentialsError } from "@/domain/errors/invalid-credentials-error";
-import { UnexpectedError } from "@/domain/errors/unexpected-error";
+import { HttpPostClient, HttpStatusCode } from "@/data/protocols/http/";
+import { InvalidCredentialsError, UnexpectedError } from "@/domain/errors/";
 import { AccountModel } from "@/domain/models/AccountModel";
-import {
-  Authentication,
-  AuthenticationParams,
-} from "@/domain/usecases/Authentication";
+import { Authentication, AuthenticationParams } from "@/domain/usecases/";
 export class RemoteAuthentication implements Authentication {
   constructor(
     private readonly url: string,
