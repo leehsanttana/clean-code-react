714e068e468e9ed75e024aa362b24620a5b789f6
Leonam Santana <leonamsanttana@gmail.com>
2022-03-31 15:24:56 -0300

    feat: add app router

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -4,5 +4,6 @@ import Input from "./input/input";
 import LoginHeader from "./login-header/login-header";
 import Logo from "./logo/logo";
 import Spinner from "./spinner/spinner";
+import Router from "./router/router";
 
-export { Footer, FormStatus, Input, LoginHeader, Logo, Spinner };
+export { Footer, FormStatus, Input, LoginHeader, Logo, Spinner, Router };
