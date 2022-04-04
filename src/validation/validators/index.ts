f54325e3c9e9a27dbcd76558be0f576469624340
Leonam Santana <leonamsanttana@gmail.com>
2022-04-04 10:37:33 -0300

    feat: ensure ValidationBuilder has a required helper

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,7 @@
+import { ValidationComposite } from "./validation-composite/validation-composite";
+import { ValidationBuilder } from "./builder/validation-builder";
+import { EmailValidation } from "./email/email-validation";
+import { MinLengthValidation } from "./min-length/min-length-validation";
+import { RequiredFieldValidation } from "./required-field/required-field-validation";
+
+export { ValidationComposite, ValidationBuilder, EmailValidation, MinLengthValidation, RequiredFieldValidation };
