c3b46199abc44c322fe031af6925947f8aae563c
Leonam Santana <leonamsanttana@gmail.com>
2022-04-04 08:25:41 -0300

    feat: ensure EmailValidation returns error if email is invalid

--- src/validation/errors/index.ts
+++ src/validation/errors/index.ts
@@ -1,3 +1,4 @@
 import { RequiredFieldError } from "./required-field-error";
+import { InvalidFieldError } from "./invalid-field-error";
 
-export { RequiredFieldError };
+export { RequiredFieldError, InvalidFieldError };
