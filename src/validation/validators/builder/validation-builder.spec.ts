6f5e724894bdbf4dd1ae8783f058366e53a0ab25
Leonam Santana <leonamsanttana@gmail.com>
2022-04-04 10:41:34 -0300

    test: ensure ValidationBuilder has an email helper

--- src/validation/validators/builder/validation-builder.spec.ts
+++ src/validation/validators/builder/validation-builder.spec.ts
@@ -6,4 +6,9 @@ describe("ValidationBuilder", () => {
     const validations = ValidationBuilder.field("any_Field").required().build();
     expect(validations).toEqual([new RequiredFieldValidation("any_Field")]);
   });
+
+  test("Should return EmailValidation ", () => {
+    const validations = ValidationBuilder.field("any_Field").email().build();
+    expect(validations).toEqual([new RequiredFieldValidation("any_Field")]);
+  });
 });
