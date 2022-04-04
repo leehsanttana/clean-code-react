b857478877732e286c6650fe376ccbf35366deb7
Leonam Santana <leonamsanttana@gmail.com>
2022-04-04 10:46:03 -0300

    feat: ensure ValidationBuilder has an minLength helper

--- src/validation/validators/builder/validation-builder.ts
+++ src/validation/validators/builder/validation-builder.ts
@@ -1,6 +1,5 @@
 import { FieldValidation } from "@/validation/protocols/field-validation";
-import { RequiredFieldValidation } from "@/validation/validators";
-import { EmailValidation } from "../email/email-validation";
+import { RequiredFieldValidation, EmailValidation, MinLengthValidation } from "@/validation/validators";
 
 export class ValidationBuilder {
   private constructor(private readonly fieldName: string, private readonly validations: FieldValidation[]) {}
@@ -16,6 +15,11 @@ export class ValidationBuilder {
 
   email(): ValidationBuilder {
     this.validations.push(new EmailValidation(this.fieldName));
+    return this;
+  }
+
+  min(length: number): ValidationBuilder {
+    this.validations.push(new MinLengthValidation(this.fieldName, length));
     return this;
   }
 
